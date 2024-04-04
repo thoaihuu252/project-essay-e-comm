4a850e286011ba1c65a9c2b508c68612ee12ffa5
PhamHuuThoai <thoaihuu252@gmail.com>
2024-04-04 21:32:18 +0700

    Build App

---
+++ app/src/main/java/com/nhom4/project_essay_e_comm/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.nhom4.project_essay_e_comm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nhom4.project_essay_e_comm.ui.theme.ProjectessayecommTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProjectessayecommTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProjectessayecommTheme {
+        Greeting("Android")
+    }
+}
